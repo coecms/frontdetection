6015b64b568faa9c062a27038ae812dc3fde9c7f
C. Carouge <c.carouge@unsw.edu.au>
2021-09-15 07:57:24 +1000

    Update to test of zeropoints functions

--- test_json.py
+++ test_json.py
@@ -3,13 +3,29 @@ import pandas as pd
 
 with open(f"test_zeropoints.json", "r") as testfile:
     jdump = json.load(testfile)
-df = pd.DataFrame(jdump["along_lat"], columns=["lat", "lon"])
-print(df)
-
 with open(f"test_zeropoints2.json", "r") as testfile:
     jdump_oldvers = json.load(testfile)
-df_oldvers = pd.DataFrame(jdump_oldvers["along_lat"], columns=["lat", "lon"])
-sort_oldvers = df_oldvers.sort_values(by=["lat", "lon"], ascending=[False, True])
-print(sort_oldvers)
 
-print(sort_oldvers.equals(df))
+
+def df_peroutput(newdump, olddump, out_type):
+    """
+    newdump: json output from the new zeropoints version
+    olddump: json output from the old zeropoints version
+    out_type: ["along_lat" | "along_lon"]
+    """
+
+    df = pd.DataFrame(newdump[out_type], columns=["lat", "lon"])
+
+    df_oldvers = pd.DataFrame(olddump[out_type], columns=["lat", "lon"])
+    sort_oldvers = df_oldvers.sort_values(by=["lat", "lon"], ascending=[False, True])
+
+    return (df, sort_oldvers)
+
+
+new_along_lat, old_along_lat = df_peroutput(jdump, jdump_oldvers, "along_lat")
+new_along_lon, old_along_lon = df_peroutput(jdump, jdump_oldvers, "along_lon")
+
+print(new_along_lon)
+print(old_along_lon)
+print(f"along_lat comparison: ", old_along_lat.equals(new_along_lat))
+print(f"along_lon comparison: ", old_along_lon.equals(new_along_lon))
