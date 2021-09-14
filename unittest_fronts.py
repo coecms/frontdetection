c26b634ec517ce827b4f386a0de0751df34745d7
Holger Wolff <holger.wolff@monash.edu>
2021-09-14 12:55:33 +1000

    add tests for zeropoint

--- unittest_fronts.py
+++ unittest_fronts.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+from scipy.signal.ltisys import ZerosPolesGain
 import xarray as xr
 from fronts import *
 
@@ -61,14 +62,92 @@ class Test_Saturation_Pressure(unittest.TestCase):
         t_in = 21.0 + 273.15
         self.assertAlmostEqual(calculate_saturation_pressure(t_in), 24.81888, places=4)
 
-class Test_Dewpoint_Temperature(unittest.TestCase):
-    def test_dewpoint_temperature(self):
-        self.assertFalse(True, msg="Dewpoint Temperature Tests not yet implemented")
+
+# class Test_Dewpoint_Temperature(unittest.TestCase):
+#     def test_dewpoint_temperature(self):
+#         self.assertFalse(True, msg="Dewpoint Temperature Tests not yet implemented")
 
 
-class Test_Relative_Humidity(unittest.TestCase):
-    def test_relative_humidity(self):
-        self.assertFalse(True, msg="Relative Humidity Test not yet implemented")
+# class Test_Relative_Humidity(unittest.TestCase):
+#     def test_relative_humidity(self):
+#         self.assertFalse(True, msg="Relative Humidity Test not yet implemented")
+
+class Test_Zeropoints(unittest.TestCase):
+
+    def create_data(self, data):
+        d = np.array(data)
+        d1 = np.arange(d.shape[0])
+        d2 = np.arange(d.shape[1])
+        return d, d1, d2
+
+    def test_no_zeropoints(self):
+        d, d1, d2 = self.create_data(
+            [[1, 1, 1], 
+             [1, 1, 1],
+             [1, 1, 1]])
+        self.assertEqual(zeropoints(d, d1, d2).size, 0)
+
+    def test_zeropoints_on_grid(self):
+        d, d1, d2 = self.create_data(
+            [[-1, 0, 1], 
+             [-1, 0, 1],
+             [-1, 0, 1]]
+        )
+        res = zeropoints(d, d1, d2)
+        expected = np.array([[0, 1], [1, 1]])
+        np.testing.assert_allclose(res, expected)
+
+    def test_zeropoints_between_grid(self):
+        d, d1, d2 = self.create_data(
+            [[-1, 1, 1], 
+             [-1, 1, 1],
+             [-1, 1, 1]]
+        )
+        res = zeropoints(d, d1, d2)
+        expected = np.array([[0, 0.5], [1, 0.5]])
+        np.testing.assert_allclose(res, expected)
+
+    def test_zeropoints_on_grid_y(self):
+        d, d1, d2 = self.create_data(
+            [[-1, -1, -1], 
+             [ 0,  0,  0],
+             [ 1,  1,  1]]
+        )
+        res = zeropoints(d, d1, d2)
+        expected = np.array([[1, 0], [1, 1]])
+        np.testing.assert_allclose(res, expected)
+
+    def test_zeropoints_between_grid_y(self):
+        d, d1, d2 = self.create_data(
+            [[-1, -1, -1], 
+             [1, 1, 1],
+             [1, 1, 1]]
+        )
+        res = zeropoints(d, d1, d2)
+        expected = np.array([[0.5, 0], [0.5, 1]])
+        np.testing.assert_allclose(res, expected)
+
+    def test_zeropoints_fractional(self):
+        d, d1, d2 = self.create_data(
+            [[-1, -1, -1], 
+             [3, 3, 3],
+             [1, 1, 1]]
+        )
+        res = zeropoints(d, d1, d2)
+        expected = np.array([[0.25, 0], [0.25, 1]])
+        np.testing.assert_allclose(res, expected)
+
+    def test_zeropoints_diagonal(self):
+        d, d1, d2 = self.create_data(
+            [[-3, -1, 2], 
+             [-2, 0, 1],
+             [1, 1, 4]]
+        )
+        res = zeropoints(d, d1, d2)
+        expected = np.array([[0, 4/3], [1, 1], [5/3, 0]])
+        np.testing.assert_allclose(res, expected)
+
+
 
 if __name__ == '__main__':
     unittest.main()
